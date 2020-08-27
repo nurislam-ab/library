0e6c52995956e619f019a9b3227e77841dd000fb
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-27 12:24:52 -0500

    Fixed js linters and linters

--- scripts.js
+++ scripts.js
@@ -7,66 +7,60 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  let title = document.getElementById('title').value;
-  console.log(title);
-  
-  let author = document.getElementById('author').value;
-  console.log(author);
-  let pages = document.getElementById('pages').value;
-  console.log(pages);
-  let read = document.getElementById('read').checked;
-  console.log(read);
-
-  let book = new Book(title, author, pages, read);
-  myLibrary.push(book);
-  render();
+function checkBooks(library) {
+  return library.length === 0 ? 'There are no books' : '';
 }
 
 function render() {
-
-  let bookList = document.getElementById('book-list');
+  const bookList = document.getElementById('book-list');
   bookList.innerHTML = checkBooks(myLibrary);
 
   myLibrary.forEach((element, index) => {
-    let row = document.createElement('tr');
+    const row = document.createElement('tr');
     row.id = index;
-    let checked = element.read ? 'checked' : '';
+    const checked = element.read ? 'checked' : '';
 
     row.innerHTML = `<td>${element.title}</td> <td>${element.author}</td>\n <td>${element.pages}</td> <td><input onclick="updateBook(${index})" type="checkbox" name="read" id="read" ${checked}></td>\n`;
     row.innerHTML += `<td><button onclick="removeBook(${index})"=>Delete Book</button></td>`;
     bookList.appendChild(row);
-  })
+  });
 }
 
-function checkBooks (library) {
-  return library.length == 0 ? 'There are no books': '';
-}
-
-function updateBook(index) {
-  let currentBook = myLibrary[index];
-  let currentStatus = currentBook.read;
-
-  newStatus = currentStatus ? false : true;
-  currentBook.read = newStatus;
-}
-
-window.onload = function () {
+function addBookToLibrary() { // eslint-disable-line no-unused-vars
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').checked;
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
   render();
 }
 
-function removeBook(index) {
-  let firstPart = myLibrary.slice(0, index); 
-  let secondPart = myLibrary.slice(index + 1, myLibrary.length); 
+
+function updateBook(index) { // eslint-disable-line no-unused-vars
+  const currentBook = myLibrary[index];
+  const currentStatus = currentBook.read;
+
+  const newStatus = !currentStatus;
+  currentBook.read = newStatus;
+}
+
+window.onload = () => {
+  render();
+};
+
+function removeBook(index) { // eslint-disable-line no-unused-vars
+  const firstPart = myLibrary.slice(0, index);
+  const secondPart = myLibrary.slice(index + 1, myLibrary.length);
   myLibrary = firstPart.concat(secondPart);
   render();
 }
 
-function showForm() {
-  let form = document.getElementById('add-new-book-form');
-  if (form.style.display === "none") {
-    form.style.display = "block";
+function showForm() { // eslint-disable-line no-unused-vars
+  const form = document.getElementById('add-new-book-form');
+  if (form.style.display === 'none') {
+    form.style.display = 'block';
   } else {
-    form.style.display = "none";
+    form.style.display = 'none';
   }
 }
