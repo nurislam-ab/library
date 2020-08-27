f4b0f61618b956c4ebf86e5997bd48b41e5517d2
Nurislam Ab <you@example.com>
2020-08-27 20:12:19 +0500

    Add base book constructor and addBookToLibrary method

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.getElementById('title').value;
+  console.log(title);
+  
+  let author = document.getElementById('author').value;
+  console.log(author);
+  let pages = document.getElementById('pages').value;
+  console.log(pages);
+  let read = document.getElementById('read').checked;
+  console.log(read);
+
+  let book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
